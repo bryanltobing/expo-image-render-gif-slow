deea4cee7e38556119a2bb71c39d1138bd674f73
Bryan Lumbantobing <bryantobing0@gmail.com>
2023-10-30 13:29:11 +0800

    feat: add reproduceable example

--- App.tsx
+++ App.tsx
@@ -1,16 +1,80 @@
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Image as ExpoImage } from "expo-image";
+import { Image as RnImage } from "react-native";
+import { useState } from "react";
 
 const Stack = createStackNavigator();
 
-const Home = () => {
+const Home = ({ navigation }: any) => {
+  const [isRnImage, setIsRnImage] = useState(false);
+
+  const Image = isRnImage ? RnImage : (ExpoImage as any);
+
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Button
+        title="Navigate to notifications screen"
+        onPress={() => navigation.navigate("Notifications")}
+      />
+      <Button
+        title={`Current: ${isRnImage ? "Rn Image" : "Expo Image"} - Use ${
+          isRnImage ? "Expo Image" : "Rn Image"
+        } instead`}
+        onPress={() => setIsRnImage((prev) => !prev)}
+      />
+      <Image
+        source={{
+          uri: "https://i0.wp.com/www.galvanizeaction.org/wp-content/uploads/2022/06/Wow-gif.gif?fit=450%2C250&ssl=1",
+        }}
+        style={{ width: 300, height: 200 }}
+        contentFit="contain"
+        contentPosition="left"
+      />
+      <Image
+        source={{
+          uri: "https://i0.wp.com/www.galvanizeaction.org/wp-content/uploads/2022/06/Wow-gif.gif?fit=450%2C250&ssl=1",
+        }}
+        style={{ width: 300, height: 200 }}
+        contentFit="contain"
+        contentPosition="left"
+      />
+      <Image
+        source={{
+          uri: "https://i0.wp.com/www.galvanizeaction.org/wp-content/uploads/2022/06/Wow-gif.gif?fit=450%2C250&ssl=1",
+        }}
+        style={{ width: 300, height: 200 }}
+        contentFit="contain"
+        contentPosition="left"
+      />
+      <Image
+        source={{
+          uri: "https://i0.wp.com/www.galvanizeaction.org/wp-content/uploads/2022/06/Wow-gif.gif?fit=450%2C250&ssl=1",
+        }}
+        style={{ width: 300, height: 200 }}
+        contentFit="contain"
+        contentPosition="left"
+      />
+      <Image
+        source={{
+          uri: "https://i0.wp.com/www.galvanizeaction.org/wp-content/uploads/2022/06/Wow-gif.gif?fit=450%2C250&ssl=1",
+        }}
+        style={{ width: 300, height: 200 }}
+        contentFit="contain"
+        contentPosition="left"
+      />
+      <Image
+        source={{
+          uri: "https://i0.wp.com/www.galvanizeaction.org/wp-content/uploads/2022/06/Wow-gif.gif?fit=450%2C250&ssl=1",
+        }}
+        style={{ width: 300, height: 200 }}
+        contentFit="contain"
+        contentPosition="left"
+      />
+    </ScrollView>
   );
 };
 const Notifications = () => {
@@ -35,9 +99,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
   },
 });
